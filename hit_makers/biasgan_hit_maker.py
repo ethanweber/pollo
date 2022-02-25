5b3eaaa19c210c67b6232277660b4e8338265653
Ethan Weber <eweb0124@gmail.com>
2022-02-25 18:24:54 +0000

    adding more settings

--- hit_makers/biasgan_hit_maker.py
+++ hit_makers/biasgan_hit_maker.py
@@ -8,14 +8,19 @@ class BiasganHitMaker(HitMaker):
     def __init__(self):
         super().__init__()
         self.buttons = None
+        self.quality_control_type = "ground_truth"
+        self.quality_control_min_percent_correct = 0.8
 
     def get_hit(self,
                 examples,
                 gt_query_examples,
                 query_examples):
         hit = super().get_hit()
-        hit["settings"]["type"] = "pickn"
+        hit["settings"]["type"] = "buttons"
         hit["settings"]["buttons"] = self.buttons
+        hit["settings"]["quality_control"] = {}
+        hit["settings"]["quality_control"]["type"] = self.quality_control_type
+        hit["settings"]["quality_control"]["min_percent_correct"] = self.quality_control_min_percent_correct
 
         hit["EXAMPLES"] = examples
         hit["GT_HIDDEN_EXAMPLES"] = gt_query_examples
--- hit_makers/hit_maker.py
+++ hit_makers/hit_maker.py
@@ -7,16 +7,28 @@ class HitMaker():
     """
 
     def __init__(self,
-                 min_seconds_per_query_example=1):
+                 min_seconds_per_query_example=1,
+                 image_height=300,
+                 image_max_width=500):
         self.min_seconds_per_query_example = min_seconds_per_query_example
+        self.image_height = image_height
+        self.image_max_width = image_max_width
 
     def get_hit(self):
         hit = {}
         hit["settings"] = {}
         hit["settings"]["min_seconds_per_query_example"] = self.min_seconds_per_query_example
+        hit["settings"]["image_height"] = self.image_height
+        hit["settings"]["image_max_width"] = self.image_max_width
         return hit
 
+    def validate_hit(self, hit):
+        assert hit["settings"]["quality_control"]["type"] in ["ground_truth", "consistency_check"]
+        assert 0.0 <= hit["settings"]["quality_control"]["min_percent_correct"] <= 1.0
+
     def save(self, hit_name, hit):
-        filename = goat.pjoin(goat.io_utils.get_git_root(os.path.abspath(__file__)), "static/data/hits", hit_name + ".json")
+        self.validate_hit(hit)
+        filename = goat.pjoin(goat.io_utils.get_git_root(os.path.abspath(__file__)),
+                              "static/data/hits", hit_name + ".json")
         goat.make_dir(filename)
         goat.write_to_json(filename, hit)
