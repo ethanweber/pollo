0d45930e2dac0e558047f8d1cae9c5525ba2c0c3
Ethan Weber <ethanweber@berkeley.edu>
2024-01-28 05:06:38 +0000

    chicken images for example

--- pollo/utils/hit.py
+++ pollo/utils/hit.py
@@ -1,27 +1,20 @@
-import os
-
-from os.path import join as pjoin
-import pollo.utils.io as io_utils
+from dataclasses import dataclass
 
 
+@dataclass
 class HitMaker:
     """
+    HIT class for storing HIT settings.
     """
 
-    def __init__(self,
-                 min_seconds_per_query_example=1,
-                 expected_seconds_per_query_example=10,
-                 image_height=300,
-                 image_max_width=500,
-                 endpoint="https://pollo.ethanweber.me"):
-        self.min_seconds_per_query_example = min_seconds_per_query_example
-        self.expected_seconds_per_query_example = expected_seconds_per_query_example
-        self.image_height = image_height
-        self.image_max_width = image_max_width
-        self.endpoint = endpoint
-        # self.project_name = None # needs to be set from a child class
+    min_seconds_per_query_example: int = 1
+    expected_seconds_per_query_example: int = 10
+    image_height: int = 300
+    image_max_width: int = 500
+    endpoint: str = "https://pollo.ethanweber.me"
 
-    def get_hit(self):
+    def get_default_hit(self):
+        """Get a hit with default settings."""
         hit = {}
         hit["settings"] = {}
         hit["settings"]["min_seconds_per_query_example"] = self.min_seconds_per_query_example
@@ -29,16 +22,3 @@ class HitMaker:
         hit["settings"]["image_height"] = self.image_height
         hit["settings"]["image_max_width"] = self.image_max_width
         return hit
-
-    def validate_hit(self, hit):
-        assert hit["settings"]["quality_control"]["type"] in ["ground_truth", "consistency_check"]
-        assert 0.0 <= hit["settings"]["quality_control"]["min_percent_correct"] <= 1.0
-
-    def save(self, hit_name, hit):
-        self.validate_hit(hit)
-        # assert self.project_name is not None
-        filename = pjoin(io_utils.get_git_root(os.path.abspath(__file__)),
-                         "static/data/hits", hit_name + ".json")
-        assert not os.path.exists(filename), filename
-        io_utils.make_dir(filename)
-        io_utils.write_to_json(filename, hit)
