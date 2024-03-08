808ff64a32e1dd01e98d160907fa56f80e278e0c
Ethan Weber <ethanweber@berkeley.edu>
2024-03-08 21:24:00 +0000

    video template with image prompts

--- pollo/example_templates.py
+++ pollo/example_templates.py
@@ -40,10 +40,37 @@ class VideoExampleTemplate:
         html_str += """</div>"""
         return html_str
 
+class VideoExampleTemplateWithImagePrompts:
+    name: str = "VideoExampleTemplateWithImagePrompts"
+
+    def get_div(self, data):
+        html_str = ""
+        html_str = """<div>"""
+        html_str += """<div class="PickNRow">"""
+        for i, image_url in enumerate(data["image_prompts"]):
+            html_str += f"""<div class="PickNRowItem" width="25%">"""
+            html_str += f"""<div class="img-overlay-wrap" width="100%">"""
+            html_str += f"""<img height=300 src="{image_url}">"""
+            html_str += """</div>"""
+            html_str += "</div>"
+        html_str += """</div>"""
+        html_str += """<div class="PickNRow">"""
+        for i, choice in enumerate(data["choices"]):
+            html_str += f"""<div class="PickNRowItem" width="25%">"""
+            html_str += f"""<div class="img-overlay-wrap" width="100%">"""
+            html_str += f"""<video height=300 src="{choice}" controls="controls" preload="metadata" width="100%">"""
+            html_str += """</div>"""
+            html_str += f"""<button style="width: 25%" type="button"> {i} </button>"""
+            html_str += "</div>"
+        html_str += """</div>"""
+        html_str += """</div>"""
+        return html_str
+
 
 def get_template_instance(template_name):
     templates = {
         "ImageExampleTemplate": ImageExampleTemplate,
         "VideoExampleTemplate": VideoExampleTemplate,
+        "VideoExampleTemplateWithImagePrompts": VideoExampleTemplateWithImagePrompts,
     }
     return templates.get(template_name, None)()
